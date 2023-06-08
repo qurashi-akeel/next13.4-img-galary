fc3e920c277bb55e6600cae7318f876c56e80a10
Iqueal <qurashiakeel786@gmail.com>
2023-06-08 16:36:36 +0530

    front page text styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-10">
-      <h1 className="font-bold text-4xl my-20 max-w-xl">
+      <h1 className="font-bold text-2xl md:text-5xl my-20 max-w-2xl bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent text-center">
         Search for any image with your favourite fetching method, and observe
         the difference
       </h1>
