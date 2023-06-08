56b313c128047226c249a6bd54c1fe701f8287d8
Iqueal <qurashiakeel786@gmail.com>
2023-06-08 16:47:55 +0530

    added og image

--- src/app/(csr)/search/page.tsx
+++ src/app/(csr)/search/page.tsx
@@ -7,6 +7,14 @@ export const metadata = {
 const SearchPage = () => {
   return (
     <div className="flex flex-col items-center p-10">
+      <h1 className="text-2xl font-bold text-center my-4">ISR</h1>
+      <p className="md:mx-10 border p-2 mx-5 lg:w-1/2 text-justify">
+        This page fetches data <strong>Client Side</strong>, in order to not
+        leak api credentials, the request is sent to{' '}
+        <strong>nextjs API route handler</strong> that runs on the server. This
+        route handler then fetches the data from the images API and returns it
+        to the client.
+      </p>
       <SearchComponent />
     </div>
   );
